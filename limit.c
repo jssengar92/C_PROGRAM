dbf369d31fcd1bb6be4fc7a3f679845133628fa3
Jyoti Singh <jssengar92@gmail.com>
2018-03-11 15:18:34 +0530

    program to find limit of program

---
+++ limit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+int main()
+{
+	printf("Range of integer value:%d to %d \n",INT_MIN,INT_MAX);
+	printf("Range of char value:%d to %d \n",CHAR_MIN,CHAR_MAX);
+	printf("Range of float value:%e to %e \n",FLT_MIN,FLT_MAX);
+	printf("Range of double value:%e to %e \n",DBL_MIN,DBL_MAX);
+	printf("Range of long double value:%e to %e \n",LDBL_MIN,LDBL_MAX);
+	//printf("Range of integer value:%d to %d \n",LINT_MIN,LINT_MAX);
+}
+	
